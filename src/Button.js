ab8c5aa034b8195f9a85c7b39053a4d8422f7d58
yangshuting <1253462178@qq.com>
2017-12-23 16:13:56 +0800

    init framework

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Button.css'
+import {Navbar, Button} from 'react-bootstrap'
+
+class Demo extends Component{
+    render(){
+        return <div className="red">Bootstrap css style <Navbar /> <Button>编辑</Button></div>
+    }
+}
+
+export default Demo
